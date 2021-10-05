0bf45c6580f9d7a06938daffd6f1baac0edc0035
Daniel Cregg <danielcregg@users.noreply.github.com>
2021-10-05 15:29:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# eclipse-java-project-template
+This is a template for an Eclipse Java Project
---
+++ src/ie/gmit/testpackage/Main.java
@@ -0,0 +1,13 @@
+package ie.gmit.testpackage;
+
+/**
+ * The Main class holds the main method.
+ */
+public class Main {
+	
+	// Main method containing print statement.
+	public static void main(String[] args) {
+		System.out.println("This is the main method, inside the Main calss.");
+	} // End main method
+	
+} // End Main Class
